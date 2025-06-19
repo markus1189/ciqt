14a27ba1d55ac127f78d3be6bf02028ec246e6fe
Markus Hauck <markus1189@gmail.com>
2025-06-19 16:21:55 +0200

    fix: add apps output to flake for nix shell support

--- flake.nix
+++ flake.nix
@@ -14,10 +14,30 @@
 
     in {
       overlay = (final: prev: {
-        ciqt = final.haskellPackages.callCabal2nix "ciqt" ./. { };
+        ciqt = (final.haskellPackages.callCabal2nix "ciqt" ./. { }).overrideAttrs (oldAttrs: {
+          postInstall = ''
+            ${oldAttrs.postInstall or ""}
+
+            # Generate shell completion scripts
+            mkdir -p $out/share/bash-completion/completions
+            mkdir -p $out/share/zsh/site-functions
+            mkdir -p $out/share/fish/vendor_completions.d
+
+            $out/bin/ciqt --bash-completion-script $out/bin/ciqt > $out/share/bash-completion/completions/ciqt
+            $out/bin/ciqt --zsh-completion-script $out/bin/ciqt > $out/share/zsh/site-functions/_ciqt
+            $out/bin/ciqt --fish-completion-script $out/bin/ciqt > $out/share/fish/vendor_completions.d/ciqt.fish
+          '';
+        });
       });
       packages = forAllSystems (system: { ciqt = nixpkgsFor.${system}.ciqt; });
       defaultPackage = forAllSystems (system: self.packages.${system}.ciqt);
+      apps = forAllSystems (system: {
+        ciqt = {
+          type = "app";
+          program = "${self.packages.${system}.ciqt}/bin/ciqt";
+        };
+        default = self.apps.${system}.ciqt;
+      });
       checks = self.packages;
       devShell = forAllSystems (system:
         let
