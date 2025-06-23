ba443a74133cede8b1aaa14074ea8cba98d7dd19
Markus Hauck <markus1189@gmail.com>
2025-06-23 09:10:10 +0200

    Remove broken postInstall

--- flake.nix
+++ flake.nix
@@ -14,20 +14,7 @@
 
     in {
       overlay = (final: prev: {
-        ciqt = (final.haskellPackages.callCabal2nix "ciqt" ./. { }).overrideAttrs (oldAttrs: {
-          postInstall = ''
-            ${oldAttrs.postInstall or ""}
-
-            # Generate shell completion scripts
-            mkdir -p $out/share/bash-completion/completions
-            mkdir -p $out/share/zsh/site-functions
-            mkdir -p $out/share/fish/vendor_completions.d
-
-            $out/bin/ciqt --bash-completion-script $out/bin/ciqt > $out/share/bash-completion/completions/ciqt
-            $out/bin/ciqt --zsh-completion-script $out/bin/ciqt > $out/share/zsh/site-functions/_ciqt
-            $out/bin/ciqt --fish-completion-script $out/bin/ciqt > $out/share/fish/vendor_completions.d/ciqt.fish
-          '';
-        });
+        ciqt = (final.haskellPackages.callCabal2nix "ciqt" ./. { });
       });
       packages = forAllSystems (system: { ciqt = nixpkgsFor.${system}.ciqt; });
       defaultPackage = forAllSystems (system: self.packages.${system}.ciqt);
